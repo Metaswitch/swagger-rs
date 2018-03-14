5f1f8bc151c42938cd168017f9d996baa2fb4c79
Tom Avery <tom.avery@metaswitch.com>
2018-03-14 11:46:10 +0000

    fixed imports

--- src/composites.rs
+++ src/composites.rs
@@ -1,113 +1,112 @@
+use std::io;
+use hyper::server::{Service, NewService};
+use hyper::Request;
+use futures::Future;
+use context::Context;
 
-use std::collections::HashMap;
-use hyper::server::{NewService, Service};
-use futures::future::{Future, err};
-use std::io::Error;
-
-pub struct CompositeService{
-    new_service_1 : NewService1;
-    new_service_2 : NewService2;
+pub trait HasPath {
+    fn path(&self) -> &str;
 }
 
-impl Service for CompositeService {
-    type Request = S;
-    type Response = T;
-    type Error = U;
-    type Future = Box<Future<Item = T, Error = U>>;
-    fn call(&self, req: Self::Request) -> Self::Future {
-        self.new_services
-            .get("base_path")
-            .unwrap()
-            .new_service()
-            .unwrap()
-            .call(req)
+impl HasPath for Request {
+    fn path(&self) -> &str {
+        self.path()
     }
 }
 
-struct NewService1 {}
-
-struct Service1 {}
-
-impl NewService for NewService1 {
-    type Request = ();
-    type Response = ();
-    type Error = ();
-    type Instance = Box<
-        Service<
-            Request = (),
-            Response = (),
-            Error = (),
-            Future = Box<Future<Item = (), Error = ()>>,
-        >,
-    >;
-    fn new_service(&self) -> Result<Self::Instance, Error> {
-        Ok(Box::new(Service1 {}))
+impl HasPath for (Request, Context) {
+    fn path(&self) -> &str {
+        self.0.path()
     }
 }
 
-impl Service for Service1 {
-    type Request = ();
-    type Response = ();
-    type Error = ();
-    type Future = Box<Future<Item = (), Error = ()>>;
-    fn call(&self, req: Self::Request) -> Self::Future {
-        Box::new(err(()))
+pub trait BoxedNewService<U, V, W> {
+    fn boxed_new_service(
+        &self,
+    ) -> Box<Service<Request = U, Response = V, Error = W, Future = Box<Future<Item = V, Error = W>>>>;
+}
+
+impl<T, U, V, W> BoxedNewService<U, V, W> for T
+where
+    T: NewService<Request = U, Response = V, Error = W>,
+    T::Instance: Service<Future = Box<Future<Item = V, Error = W>>>
+        + Sized
+        + 'static,
+{
+    fn boxed_new_service(
+        &self,
+    ) -> Box<Service<Request = U, Response = V, Error = W, Future = Box<Future<Item = V, Error = W>>>> {
+        let new_service = self.new_service().unwrap();
+        Box::new(new_service)
     }
 }
 
-struct NewService2 {}
+pub struct CompositeNewService<U: HasPath, V, W>(Vec<(&'static str, Box<BoxedNewService<U, V, W>>)>);
 
-struct Service2 {}
+impl<U: HasPath, V, W> CompositeNewService<U, V, W> {
+    pub fn new() -> Self {
+        CompositeNewService(Vec::new())
+    }
 
-impl NewService for NewService2 {
-    type Request = ();
-    type Response = ();
-    type Error = ();
-    type Instance = Box<
-        Service<
-            Request = (),
-            Response = (),
-            Error = (),
-            Future = Box<Future<Item = (), Error = ()>>,
-        >,
-    >;
-    fn new_service(&self) -> Result<Self::Instance, Error> {
-        Ok(Box::new(Service2 {}))
+    pub fn append_new_service(
+        &mut self,
+        base_path: &'static str,
+        new_service: Box<BoxedNewService<U, V, W>>,
+    ) {
+        self.0.push((base_path, new_service));
     }
 }
 
-impl Service for Service2 {
-    type Request = ();
-    type Response = ();
-    type Error = ();
-    type Future = Box<Future<Item = (), Error = ()>>;
-    fn call(&self, req: Self::Request) -> Self::Future {
-        Box::new(err(()))
-    }
-}
-
-fn test() {
-    let mut hash_map: HashMap<
-            &'static str,
-            Box<
-                NewService<
-                    Request = (),
-                    Response = (),
-                    Error = (),
-                    Instance = Box<
-                        Service<
-                            Request = (),
-                            Response = (),
-                            Error = (),
-                            Future = Box<Future<Item = (), Error = ()>>,
-                        >,
-                    >,
-                >,
+pub struct CompositeService<U: HasPath, V, W>(
+    Vec<
+        (&'static str,
+         Box<
+            Service<
+                Request = U,
+                Response = V,
+                Error = W,
+                Future = Box<Future<Item = V, Error = W>>,
             >,
-        > = HashMap::new();
-    hash_map.insert("service1", Box::new(NewService1 {}));
-    hash_map.insert("service2", Box::new(NewService2 {}));
-    let composite_service = CompositeService { new_services: hash_map };
-    composite_service.call(());
+        >),
+    >
+);
 
+impl<U: HasPath, V, W> NewService for CompositeNewService<U, V, W> {
+    type Request = U;
+    type Response = V;
+    type Error = W;
+    type Instance = CompositeService<U, V, W>;
+
+    fn new_service(&self) -> Result<Self::Instance, io::Error> {
+        // Call new service on each entry in hashmap, and return a new hashmap
+        let mut vec: Vec<
+            (&'static str,
+             Box<
+                Service<
+                    Request = U,
+                    Response = V,
+                    Error = W,
+                    Future = Box<Future<Item = V, Error = W>>,
+                >,
+            >),
+        > = Vec::new();
+
+        for &(key, ref value) in self.0.iter() {
+            vec.push((key, value.boxed_new_service()));
+        }
+
+        Ok(CompositeService(vec))
+    }
+}
+
+impl<U: HasPath, V, W> Service for CompositeService<U, V, W> {
+    type Request = U;
+    type Response = V;
+    type Error = W;
+    type Future = Box<Future<Item = V, Error = W>>;
+
+    fn call(&self, req: Self::Request) -> Self::Future {
+
+        ((self.0)[0].1).call(req)
+    }
 }
